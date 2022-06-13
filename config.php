dd246dbaa00cc6b6a99d873af3076c6b8f8ca945
shivamshukla13jun <107310556+shivamshukla13jun@users.noreply.github.com>
2022-06-13 16:03:51 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-$conn=mysqli_connect("localhost","vrlpwvmy_get_enquiry","ci;JF^kU1JMr","vrlpwvmy_get_product_info") or die("404 Not Found");
+$conn=mysqli_connect("localhost","root","","vrlpwvmy_get_product_info") or die("404 Not Found");
 ?>
